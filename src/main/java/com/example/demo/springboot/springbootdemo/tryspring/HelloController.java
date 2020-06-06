7f8c7da72c0c5baaf94094d79ad2a2a5bd521c68
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-07 08:10:54 +0900

    H2データベースに接続

--- src/main/java/com/example/demo/springboot/springbootdemo/tryspring/HelloController.java
+++ src/main/java/com/example/demo/springboot/springbootdemo/tryspring/HelloController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 @Controller
 public class HelloController {
+
+  @Autowired
+  private HelloService helloService;
   
   @GetMapping("/hello")
   public String getHello() {
@@ -21,6 +26,19 @@ public class HelloController {
 
     model.addAttribute("sample", str);
     return "helloRequest";
+  }
+
+  @PostMapping("/hello/db")
+  public String postDbRequest(@RequestParam("text2")String str, Model model) {
+
+    int id = Integer.parseInt(str);
+    Employee employee = helloService.findOne(id);
+
+    model.addAttribute("id", employee.getEmployee_id());
+    model.addAttribute("name", employee.getEmployee_name());
+    model.addAttribute("age", employee.getAge());
+
+    return "helloResponseDB";
   }
   
 }
