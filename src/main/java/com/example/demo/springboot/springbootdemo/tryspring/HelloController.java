3f5ef55be5af6c0c6fde4d2c38d575c8ca562dc3
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-06 21:26:01 +0900

    first commit

---
+++ src/main/java/com/example/demo/springboot/springbootdemo/tryspring/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo.springboot.springbootdemo.tryspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+  
+  @GetMapping("/hello")
+  public String getHello() {
+
+    return "hello";
+  }
+}
