a93858f113c4557089e25f53a1c3fd85e3123016
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-06 21:51:01 +0900

    フォームの値を受け取る練習

--- src/main/java/com/example/demo/springboot/springbootdemo/tryspring/HelloController.java
+++ src/main/java/com/example/demo/springboot/springbootdemo/tryspring/HelloController.java
@@ -1,7 +1,11 @@
 package com.example.demo.springboot.springbootdemo.tryspring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
@@ -11,4 +15,12 @@ public class HelloController {
 
     return "hello";
   }
+
+  @PostMapping("/hello")
+  public String postRequest(@RequestParam("text1")String str, Model model) {
+
+    model.addAttribute("sample", str);
+    return "helloRequest";
+  }
+  
 }
